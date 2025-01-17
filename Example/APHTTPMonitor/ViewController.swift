5bdbb656ddb68e7a6bf053a3eeccb3dd85619b24
Antongiulio Pellegrino <a.pellegrino@ideasolutions.it>
2025-01-17 13:22:35 +0100

    Added fake requests in example in order to test the monitor

--- Example/APHTTPMonitor/ViewController.swift
+++ Example/APHTTPMonitor/ViewController.swift
@@ -14,25 +14,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var switchMonitor: UISwitch!
     @IBOutlet weak var lblStatus: UILabel!
     @IBOutlet weak var lblServerUrl: UILabel!
+    var timer : Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        lblStatus.textColor = UIColor.red
-        
     }
 
     @IBAction func switchValueChanged(_ sender: Any) {
         if switchMonitor.isOn {
+            startTimer()
             let url = APHTTPMonitor.shared().start()
             lblStatus.textColor = UIColor.systemGreen
             lblStatus.text = "Active"
             lblServerUrl.text = url
         } else {
+            stopTimer()
             APHTTPMonitor.shared().stop()
             lblStatus.textColor = UIColor.red
             lblStatus.text = "Stopped"
             lblServerUrl.text = ""
         }
+    }
+    
+    func startTimer() {
+        guard timer == nil else {return}
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(scheduleRequest), userInfo: nil, repeats: true)
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @objc func scheduleRequest() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
+        }
+        task.resume()
     }
 }
 
